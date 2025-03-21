b403f6c4a9712b80f8708926d2981abe7790c120
Sylvanna COURBIS <sycourbi@made-f0Ar5s2.clusters.42paris.fr>
2025-03-21 15:46:35 +0100

    websocketbackendgame

--- conecte_me/backend/conecte_me_backend/asgi.py
+++ conecte_me/backend/conecte_me_backend/asgi.py
@@ -2,13 +2,16 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-import chat.routing  # 🔥 Importe le fichier de routes WebSocket
+import chat.routing
+import game.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conecte_me_backend.settings')
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),  # Gère les requêtes HTTP normales
+    "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
-        URLRouter(chat.routing.websocket_urlpatterns)  # Gère les WebSockets
+        URLRouter(
+            chat.routing.websocket_urlpatterns + game.routing.websocket_urlpatterns
+        )
     ),
 })
